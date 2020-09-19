c36dfd949355103c48ffd07fe293611b336ba8ee
Iván Rico Aracil <yooutubericoo@gmail.com>
2020-09-19 14:13:40 +0200

    Update Boss.js
    Comentarios sobre mejoras en la clase Boss.ja

--- Boss.js
+++ Boss.js
@@ -4,13 +4,15 @@
 class Boss extends Opponent {
 
     constructor(game) {
-
+        // Más lógico añadir aquí bossImage y no usar el isBoss
+        // Modificar el Opponent y enviarle los param necesarios
+        // super(game, bossImage, bossImageDead, bossSpeed)
         const bossImageDead = FINAL_BOSS_PICTURE_DEAD,
         bossSpeed = FINAL_BOSS_SPEED,
-        isBoss = true;
+        isBoss = true; // En cualquier caso puedo enviar directamente true al constructor de la clase padre
 
         super(game, isBoss);
-
+        // Estas 2 líneas no serían necesarias
         this.myImageDead = bossImageDead;
         this.speed = bossSpeed;
         
